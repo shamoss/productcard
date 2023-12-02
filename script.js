e4cf4990f16f9d79f895fcb9fff1eeee375af687
shamoss <dimashamruk34@gmail.com>
2023-12-02 17:27:57 +0300

    Add index and js

---
+++ script.js
@@ -0,0 +1,24 @@
+
+class Product{
+    imgLink;
+    productName;
+    productPrice;
+
+    constructor(link,name,price){
+        this.imgLink = link;
+        this.productName = name;
+        this.productPrice = price;
+        const container = document.createElement("div");
+        container.innerHTML = `
+        <img src = "${this.imgLink}">
+        <h5>${this.productName}</h5>
+        <h6>${this.productPrice}$</h6>
+        <button>kupit</button>
+        `
+        document.body.append(container);
+    }
+
+
+}
+
+new Product("https://kartinki.pics/uploads/posts/2022-03/thumbs/1646564796_1-kartinkin-net-p-kartinki-tsvetochkov-2.jpg", "cvetochek", 20);
